6527ce998e08887f7a937079ab8ebb62292d83da
Matthieu Ciappara <ciappa_m@modulotech.fr>
2020-08-26 16:52:25 +0200

    Initialize new gem

---
+++ lib/modulorails.rb
@@ -0,0 +1,6 @@
+require "modulorails/version"
+
+module Modulorails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/modulorails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Modulorails do
+  it "has a version number" do
+    expect(Modulorails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
