a220f2c932f0c38cff82974e491ef79f66d2f248
Matthieu Ciappara <ciappa_m@modulotech.fr>
2020-09-18 10:45:55 +0200

    Limit all tasks to development environment

--- lib/modulorails/railtie.rb
+++ lib/modulorails/railtie.rb
@@ -3,14 +3,17 @@ module Modulorails
     # Binding in the Rails lifecycle. Sending data after the initialization ensures we can access
     # all gems and symbols we might have to use.
     config.after_initialize do
-      # Effectively send the data to the intranet
-      Modulorails.send_data
+      # For now, we limit everything to the development environment
+      if Rails.env.development?
+        # Effectively send the data to the intranet
+        Modulorails.send_data
 
-      # Generate a template for CI/CD
-      Modulorails.generate_ci_template
+        # Generate a template for CI/CD
+        Modulorails.generate_ci_template
 
-      # Check database configuration
-      Modulorails.check_database_config
+        # Check database configuration
+        Modulorails.check_database_config
+      end
     end
   end
 end
