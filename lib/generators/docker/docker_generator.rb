29059e86e0adc101ef53b3dd69e922f356807c70
Matthieu Ciappara <ciappa_m@modulotech.fr>
2021-04-19 18:53:54 +0200

    Add generator for dockerisation

---
+++ lib/generators/docker/docker_generator.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails/generators'
+
+class DockerGenerator < Rails::Generators::Base
+  source_root File.expand_path('templates', __dir__)
+  desc 'This generator creates Dockerfiles for an app'
+
+  def create_config_file
+    template 'Dockerfile'
+    template 'Dockerfile.prod'
+    template 'docker-compose.yml'
+    template 'docker-compose.prod.yml'
+    template 'entrypoints/docker-entrypoint.sh'
+    template 'config/database.yml'
+  rescue StandardError => e
+    $stderr.puts("[Modulorails] Error: cannot generate Docker configuration: #{e.message}")
+  end
+end
