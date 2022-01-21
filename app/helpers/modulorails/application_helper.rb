c2affd0ae23dd39fd88af32bc6b7fcdb2b165c1f
Matthieu Ciappara <ciappa_m@modulotech.fr>
2022-01-21 14:58:19 +0100

    Add Modulorails helper powered_by

---
+++ app/helpers/modulorails/application_helper.rb
@@ -0,0 +1,8 @@
+# @author Matthieu CIAPPARA <ciappa_m@modulotech.fr>
+module Modulorails::ApplicationHelper
+  def powered_by
+    link_to('https://www.modulotech.fr/', target: '_blank', class: 'modulolink') do
+      raw("Powered by modulo<span>Tech</span>")
+    end
+  end
+end
