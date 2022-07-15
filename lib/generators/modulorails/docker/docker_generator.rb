1f8541d4e14fe336c04de431e11e81cc243163d3
Matthieu Ciappara <ciappa_m@modulotech.fr>
2022-07-15 12:16:27 +0200

    Make entrypoints executable

--- lib/generators/modulorails/docker/docker_generator.rb
+++ lib/generators/modulorails/docker/docker_generator.rb
@@ -13,10 +13,14 @@ class Modulorails::DockerGenerator < Rails::Generators::Base
     template 'docker-compose.yml'
     template 'docker-compose.prod.yml'
     template 'entrypoints/docker-entrypoint.sh'
+    chmod 'entrypoints/docker-entrypoint.sh', 0755
     template 'config/database.yml'
 
     # Useless unless project is using Webpacker
-    template 'entrypoints/webpack-entrypoint.sh' if Modulorails.data.webpacker_version.present?
+    if Modulorails.data.webpacker_version.present?
+      template 'entrypoints/webpack-entrypoint.sh'
+      chmod 'entrypoints/webpack-entrypoint.sh', 0755
+    end
   rescue StandardError => e
     warn("[Modulorails] Error: cannot generate Docker configuration: #{e.message}")
   end
