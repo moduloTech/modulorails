1211b7275cf296ee1c8b0a08a41059ca44f091a0
Matthieu Ciappara <ciappa_m@modulotech.fr>
2022-09-19 14:42:42 +0200

    Add bundler-audit generator

---
+++ lib/generators/modulorails/bundleraudit/bundleraudit_generator.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails/generators'
+
+class Modulorails::BundlerauditGenerator < Rails::Generators::Base
+
+  source_root File.expand_path('templates', __dir__)
+  desc 'This generator creates a configuration for Bundler-Audit'
+
+  def create_config_files
+    bundleraudit_config_path = Rails.root.join('.bundler-audit.yml')
+    gitlab_config_path       = Rails.root.join('.gitlab-ci.yml')
+
+    template 'bundleraudit.yml', bundleraudit_config_path
+
+    return if File.read(gitlab_config_path).match?(/\s+extends:\s+.bundleraudit\s*$/)
+
+    append_file gitlab_config_path do
+      <<~YAML
+        # Scan Gemfile.lock for Common Vulnerabilities and Exposures
+        # https://en.wikipedia.org/wiki/Common_Vulnerabilities_and_Exposures
+        # https://www.cve.org/
+        bundleraudit:
+          extends: .bundleraudit
+      YAML
+    end
+  end
+
+end
