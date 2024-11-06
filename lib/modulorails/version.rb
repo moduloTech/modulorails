d4f72abfbe9581b819ec55bd5dd1c87ccb6b9e62
Matthieu Ciappara <ciappa_m@modulotech.fr>
2024-11-06 13:54:13 +0000

    Bump modulorails to 1.5.2.pre.2

--- lib/modulorails/version.rb
+++ lib/modulorails/version.rb
@@ -1,6 +1,6 @@
 module Modulorails
 
-  VERSION = '1.5.2.pre.1'.freeze
+  VERSION = '1.5.2.pre.2'.freeze
 
   # Useful to compare the current Ruby version
   COMPARABLE_RUBY_VERSION = Gem::Version.new(RUBY_VERSION)
